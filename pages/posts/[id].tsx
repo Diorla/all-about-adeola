d782376b90841450aa6c7fa7cc851b0806c8d6aa
Ade Adeola <adedotster@gmail.com>
2020-08-24 16:51:14 +0100

    Complete blog reader

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,9 @@
-import Layout from '../../components/layout'
-import { getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
-import { GetServerSideProps } from 'next'
+import Layout from "../../components/layout";
+import { getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../../components/date";
+import { GetServerSideProps } from "next";
 
 export default function Post({ postData }) {
   return (
@@ -12,21 +12,57 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1>{postData.title}</h1>
+        <div className="date">
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          className="content"
+        />
+        <div className="bottom">
+          <Link href="/blogs">
+            <a>Back</a>
+          </Link>
+        </div>
       </article>
+      <style jsx>{`
+        h1 {
+          text-align: center;
+          font-weight: bold;
+          font-size: 24px;
+        }
+        article {
+          padding: 8px;
+          font-family: Roboto, Arial;
+        }
+        .date {
+          text-align: center;
+        }
+        .content p {
+          margin-bottom: 8px;
+          line-height: 24px;
+        }
+        .content ul {
+          margin: 8px;
+        }
+        .content .content li {
+          margin: 8px;
+          list-style: inside;
+        }
+        .bottom {
+          text-align: end;
+        }
+      `}</style>
     </Layout>
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async context => {
-  const postData = await getPostData(context.params.id)
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const postData = await getPostData(context.params.id);
   return {
     props: {
-      postData
-    }
-  }
-}
+      postData,
+    },
+  };
+};
