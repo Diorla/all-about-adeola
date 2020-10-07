49331d323fbf062fd8630b033d52722a0352aed6
Ade Adeola <adedotster@gmail.com>
2020-10-07 19:12:09 +0200

    Showcase created

--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -1,39 +1,42 @@
 import Layout from "../components/layout";
-import Product from "../components/product";
+import Highlight from "../components/highlight";
 import Contact from "../components/contact";
+import styled from "styled-components";
 
 const works = [
   {
-    name: "Weblink",
-    link: "https://www.weblinkio.web.app",
-    img: "works/ade.png",
-    description:
-      "Fugiat sit commodo esse nostrud adipisicing. Amet exercitation id id commodo veniam consectetur ad labore ullamco enim. Dolor labore quis et consequat qui. Commodo cupidatat minim nisi excepteur deserunt dolore voluptate quis ut labore. Quis nulla mollit exercitation fugiat aliqua.",
-  },
-  {
     name: "Hera",
-    link: "https://www.hera.web.app",
-    img: "works/products.jpg",
-    description:
-      "Commodo nostrud ea mollit irure labore proident sunt officia dolor ut eiusmod sint cillum deserunt. Eu veniam deserunt ullamco ex minim excepteur Lorem. Cillum mollit id minim duis est veniam occaecat cupidatat qui enim pariatur magna.",
+    link: "https://hera.adeolaade.com/",
+    img: "images/hera.svg",
+    description: "Spa and massage demo website",
+  },
+  {
+    name: "Character generator",
+    link: "https://github.com/Diorla/character-generator/releases/tag/2.0.1",
+    img: "images/chargen.svg",
+    description: "Generates random attributes for fictional characters",
   },
   {
     name: "Weblink",
-    link: "https://www.weblinkio.web.app",
-    img: "works/weblink.jpg",
-    description:
-      "Non dolor tempor veniam laborum irure do amet et proident duis cupidatat. Id sit irure nostrud quis labore veniam ipsum deserunt eu aliqua in nulla proident. Nisi mollit occaecat velit sunt eiusmod aliqua veniam anim qui pariatur incididunt non. Et labore ad eu laboris nostrud ipsum elit. Anim pariatur nisi pariatur excepteur proident enim incididunt dolor mollit. Deserunt voluptate proident sint et nisi. Commodo do commodo ea Lorem aliqua ad mollit.",
+    link: "https://weblinkio.app/",
+    img: "images/linker.svg",
+    description: "Cross browser, cross platform web bookmarking",
   },
 ];
 
+const ShowcaseDiv = styled.main`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
 export default () => (
   <Layout>
-    <h2>These are some of the projets that I have worked on</h2>
-    <main>
+    <h2>These are some of my projects</h2>
+    <ShowcaseDiv>
       {works.map((item, idx) => (
-        <Product {...item} key={idx} />
+        <Highlight key={idx} {...item} />
       ))}
-    </main>
+    </ShowcaseDiv>
     <Contact />
     <style jsx>{`
       h2 {
