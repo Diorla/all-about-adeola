39834c863309f9d6f665c4d8fcddd98325c51b7c
Ade Adeola <adedotster@gmail.com>
2020-10-07 18:11:18 +0200

    bounce animation

--- components/bounce.tsx
+++ components/bounce.tsx
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import { Waypoint } from "react-waypoint";
+
+const bounceAnimation = keyframes`
+  0% {
+    transform: scale(0);
+  }
+  80% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+const HiddenDiv = styled.div`
+  transition: 2s linear;
+  transform: scale(0);
+  overflow: hidden;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+`;
+const RevealedDiv = styled.div`
+  transition: 2s linear;
+  transform: scale(0);
+  overflow: hidden;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  animation: ${bounceAnimation} 2s ease 1s 1 forwards;
+`;
 
 export interface BounceProps {
   children: React.ReactNode;
 }
 export default function Bounce({ children }: BounceProps) {
-  const id = `bounce-${Math.floor(Math.random() * Date.now())}`;
-  const revealElement = () => {
-    document.getElementById(id)?.classList.add("reveal");
-  };
+  const [reveal, setReveal] = useState(false);
 
   return (
-    <Waypoint onEnter={() => revealElement()}>
-      <div id={id} className="bounce">
-        {children}
-        <style jsx>{`
-          .bounce {
-            transition: 2s linear;
-            transform: scale(0);
-            overflow: hidden;
-          }
-          .bounce.reveal {
-            animation: 2s ease 1s 1 forwards bouncing;
-          }
-          @keyframes bouncing {
-            0% {
-              transform: scale(0);
-            }
-            80% {
-              transform: scale(1.3);
-            }
-            100% {
-              transform: scale(1);
-            }
-          }
-        `}</style>
-      </div>
+    <Waypoint onEnter={() => setReveal(true)}>
+      {reveal ? (
+        <RevealedDiv>{children}</RevealedDiv>
+      ) : (
+        <HiddenDiv>{children}</HiddenDiv>
+      )}
     </Waypoint>
   );
 }
