9db7eb793a99d61517cf90fca59859d5f28e038c
Ade Adeola <adedotster@gmail.com>
2020-11-04 11:36:01 +0100

    remove unused packages

--- components/date.tsx
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function Date(props: { dateString: string }) {
-  const date = parseISO(props.dateString)
-  return <time dateTime={props.dateString}>{format(date, 'LLLL d, yyyy')}</time>
-}
