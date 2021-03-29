742edd78fb68b39cf7f489570a16bccf7c1063ae
Ade Adeola <adedotster@gmail.com>
2021-03-29 20:29:18 +0100

    remove carousel

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,16 +36,6 @@ export default function Layout(props: { children: ReactNode; title?: string }) {
           href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
       </Head>
       <Header />
       <Wrapper>{children}</Wrapper>
