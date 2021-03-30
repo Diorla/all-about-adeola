5c084ecc4207002a43a8d996aa5cf2fb7ea6d56a
Ade Adeola <adedotster@gmail.com>
2021-03-30 22:19:10 +0100

    header links redesigned

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 import { StyledLink } from "./Link";
 import styles from "./styles";
 
@@ -8,16 +8,23 @@ const { logo } = styles;
 
 export const StyledLogo = styled(StyledLink)`
   font-family: ${logo.fontFamily};
-  font-size: ${logo.fontSize};
+  font-size: 28px;
   margin: ${logo.margin};
   padding: ${logo.padding};
+  z-index: 100000;
+  position: absolute;
+  left: 8px;
+  color: white;
+  font-weight: bold;
   &:hover {
-    text-decoration: ${logo.textDecoration};
+    text-shadow: 0 0 2px black;
+    transform: none;
+    font-weight: bolder;
   }
 `;
 
 export default () => (
   <Link href="/">
-    <StyledLogo>Adeola Ade</StyledLogo>
+    <StyledLogo>{`<Code />`}</StyledLogo>
   </Link>
 );
