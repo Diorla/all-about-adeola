d27176818d60cfcb386eff5f05fcdff089b11501
Ade Adeola <adedotster@gmail.com>
2021-04-02 13:30:54 +0100

    create animated menu button

--- components/MenuControl/index.tsx
+++ components/MenuControl/index.tsx
@@ -1,22 +1,58 @@
-import React from "react";
+import { MorphReplace } from "react-svg-morph";
 import styled from "styled-components";
-import Morph from "./Morph";
 
-export const Menu = styled.div`
-  z-index: 100000;
-  position: absolute;
-  right: 8px;
+const Menu = () => (
+  <svg width="12" fill="#fff" height="12" viewBox="0 0 24 24">
+    <path
+      d="M7 6C5.34315 6 4 7.34315 4 9H20C20 7.34315 18.6569 6 17 6H7Z"
+      fill="#fff"
+    ></path>
+    <path
+      d="M7 18C5.34315 18 4 16.6569 4 15H20C20 16.6569 18.6569 18 17 18H7Z"
+      fill="#fff"
+    ></path>
+    <path
+      d="M3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H3Z"
+      fill="#fff"
+    ></path>
+  </svg>
+);
+// const Menu = () => (
+//   <svg width="12" fill="#fff" height="12" viewBox="0 0 24 24">
+//     <path d="M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z"></path>
+//   </svg>
+// );
+
+const Close = () => (
+  <svg width="12" height="12" fill="#fff" viewBox="0 0 24 24">
+    <path
+      fill="#fff"
+      stroke="#fff"
+      strokeWidth="2"
+      d="M3,3 L21,21 M3,21 L21,3"
+    ></path>
+  </svg>
+);
+
+const StyledMorph = styled.div`
+  cursor: url(cursor/pointer.svg), pointer;
 `;
 
-export interface MenuControlProps {
+const Morph = ({
+  onClick,
+  hidden,
+}: {
   onClick: (e: React.SyntheticEvent) => void;
-  hidden: boolean;
-}
-
-export default function MenuControl({ onClick, hidden }: MenuControlProps) {
+  hidden?: boolean;
+}) => {
   return (
-    <Menu>
-      <Morph onClick={onClick} hidden={hidden} />
-    </Menu>
+    <StyledMorph onClick={onClick}>
+      <MorphReplace width={36} height={36}>
+        {hidden ? <Menu key="a" /> : <Close key="b" />}
+      </MorphReplace>
+    </StyledMorph>
   );
-}
+};
+
+export default Morph;
+// npx browserslist@latest --update-db
